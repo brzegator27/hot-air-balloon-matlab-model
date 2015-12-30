2b4d6bede8b7cb617db28bb32f67c2406c9041dc
Jakub Brzegowski <symfiz@gmail.com>
2015-12-30 20:47:58 +0100

    Added improvements to model, but still there is bug, probably when curren height is calculated. Added small modifications in model data.

--- s1.m
+++ s1.m
@@ -7,7 +7,7 @@ ro0 = 1.2250; %gêstoœæ powietrz dla temp. 20 *C jednostka: kg / m^3
 mp0 = ro0 * V; %pocz¹tkowa masa powietrza w balomie kg
 Mp = 0.028970; %masa molowa/masa cz¹steczkowa dla powietrza suchego kg / mol
 p0 = 101325; %ciœnienie powietrza na poziomie morza Pa = N / m^2
-sprP = 0.10; %sprawnoœæ palnika podgrzewaj¹cego powietrze w balonie - wspó³czynnik
+sprP = 0.08; %sprawnoœæ palnika podgrzewaj¹cego powietrze w balonie - wspó³czynnik
 mb = 113.4 + 63.5 + 22.7 + 183.7 + 340.2; %waga balonu [ kg ] koperta + koszyk + palnik + zbiorniki z gazem + 5 pasa¿erów
 G = 6.67 * 10^(-11); %Sta³a grawitacji [ m^3 / (kg * s^2) ]
 mz = 5.97219 * 10^24; %masa Ziemii [ kg ]
