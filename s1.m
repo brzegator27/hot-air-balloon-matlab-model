fc96873946f7b785aaa8c56fa069cd797b310090
Jakub Brzegowski <symfiz@gmail.com>
2015-12-30 19:47:49 +0100

    Added small changes in model. Started creating LaTeX document with used physical equations.

--- s1.m
+++ s1.m
@@ -7,7 +7,7 @@ ro0 = 1.2250; %gêstoœæ powietrz dla temp. 20 *C jednostka: kg / m^3
 mp0 = ro0 * V; %pocz¹tkowa masa powietrza w balomie kg
 Mp = 0.028970; %masa molowa/masa cz¹steczkowa dla powietrza suchego kg / mol
 p0 = 101325; %ciœnienie powietrza na poziomie morza Pa = N / m^2
-sprP = 0.1; %sprawnoœæ palnika podgrzewaj¹cego powietrze w balonie - wspó³czynnik
+sprP = 0.10; %sprawnoœæ palnika podgrzewaj¹cego powietrze w balonie - wspó³czynnik
 mb = 113.4 + 63.5 + 22.7 + 183.7 + 340.2; %waga balonu [ kg ] koperta + koszyk + palnik + zbiorniki z gazem + 5 pasa¿erów
 G = 6.67 * 10^(-11); %Sta³a grawitacji [ m^3 / (kg * s^2) ]
 mz = 5.97219 * 10^24; %masa Ziemii [ kg ]
@@ -16,6 +16,7 @@ kWymianyPow = 0.1; %Wspó³czynnik wymiany ciep³a pomiedzy powietrzem w kopercie, 
 spadekTemp = 0.0065; %Wspó³czynnik spadku temperatury w zale¿noœci od wysokoœci [ K / m ]
 g = 9.80665; %Przyspieszenie grawitacyjne na powierzchni Ziemii [ m / s^2 ]
 promienZiemii = 6360000; %promieñ Ziemii [ m ]
+kWymianyCiepla = 0.5; %Wspó³czynnik wymiany ciep³a pomiêdzy kopert¹ balonu, a otoczeniem
 
 
 
